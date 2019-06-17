aadcfd61f769e535a0ad6c2fe4c3266a5231a535
Ben Balter <ben.balter@github.com>
2019-06-17 10:19:21 -0400

    add dependabot preview bot

--- mute.rb
+++ mute.rb
@@ -4,7 +4,7 @@ require 'logger'
 
 client = Octokit::Client.new access_token: ENV["OCTOKIT_ACCESS_TOKEN"]
 since = Time.now - (60 * 20)
-ignored = %w(stale[bot] jekyllbot dependabot[bot])
+ignored = %w(stale[bot] jekyllbot dependabot[bot] dependabot-preview[bot])
 logger = Logger.new(STDOUT)
 notifications = client.notifications(since: since.iso8601)
 
