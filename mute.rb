6780069c320e4203a36d20d2f6c1fa047b377323
Ben Balter <ben.balter@github.com>
2019-06-17 10:20:41 -0400

    style

--- mute.rb
+++ mute.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'dotenv/load'
 require 'octokit'
 require 'logger'
 
-client = Octokit::Client.new access_token: ENV["OCTOKIT_ACCESS_TOKEN"]
+client = Octokit::Client.new access_token: ENV['OCTOKIT_ACCESS_TOKEN']
 since = Time.now - (60 * 20)
 ignored = %w(stale[bot] jekyllbot dependabot[bot] dependabot-preview[bot])
 logger = Logger.new(STDOUT)
@@ -12,8 +14,10 @@ logger.info "Found #{notifications.count} notification(s)"
 
 notifications.each do |notification|
   next unless notification.subject.rels[:latest_comment]
+
   comment = notification.subject.rels[:latest_comment].get.data
   next unless ignored.include?(comment.user.login)
+
   logger.info "Marking #{notification.subject.title} as read"
   client.mark_thread_as_read(notification.id)
 end
