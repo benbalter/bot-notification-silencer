c4c44056f0d99bad11e753fd9cb019de71c1f939
Ben Balter <ben.balter@github.com>
2019-11-12 15:07:33 -0800

    fix

--- mute.rb
+++ mute.rb
@@ -13,10 +13,18 @@ notifications = client.notifications(since: since.iso8601)
 logger.info "Found #{notifications.count} notification(s)"
 
 notifications.each do |notification|
+  author = notification.subject.rels[:self].get.data.user
+
+  if ignored.include?(author.login)
+    logger.info "Marking #{notification.subject.title} as read"
+    client.mark_thread_as_read(notification.id)
+    next
+  end
+
   next unless notification.subject.rels[:latest_comment]
 
   comment = notification.subject.rels[:latest_comment].get.data
-  next unless ignored.include?(comment.user.login)
+  next unless ignored.include?( comment.user.login)
 
   logger.info "Marking #{notification.subject.title} as read"
   client.mark_thread_as_read(notification.id)
