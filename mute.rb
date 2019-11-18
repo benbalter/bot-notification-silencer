eb18cfe4d22dcae7bbc35f1f6fe17a4a5035853a
Ben Balter <ben.balter@github.com>
2019-11-18 12:02:02 -0500

    add ruby version

--- mute.rb
+++ mute.rb
@@ -24,7 +24,7 @@ notifications.each do |notification|
   next unless notification.subject.rels[:latest_comment]
 
   comment = notification.subject.rels[:latest_comment].get.data
-  next unless ignored.include?( comment.user.login)
+  next unless ignored.include?(comment.user.login)
 
   logger.info "Marking #{notification.subject.title} as read"
   client.mark_thread_as_read(notification.id)
